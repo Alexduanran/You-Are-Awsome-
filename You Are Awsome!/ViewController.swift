ed84198a102c47a5775bcd877b9ec53ca8e0aba4
Alex Du <alexduanran@alexs-macbook-pro.local>
2018-08-30 17:32:38 -0400

    Initial Commit

---
+++ You Are Awsome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awsome!
+//
+//  Created by Alex Du on 8/30/18.
+//  Copyright © 2018 Alex Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
